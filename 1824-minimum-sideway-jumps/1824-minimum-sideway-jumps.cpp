dbbe8837684150952ebd1ae04fd3d70228fa7c4a
Numan Akhtar <153702609+Numan309@users.noreply.github.com>
2024-08-27 17:50:30 +0530

    Time: 175 ms (91.07%), Space: 232.7 MB (68.29%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.cpp
@@ -1,30 +1,63 @@
 class Solution {
 public:
-    int solve(vector<int> &obstacles,int curr_lane,int pos,vector<vector<int>>&dp)
-    {
-        int n = obstacles.size()-1;
+    int solve(vector<int> &obstacles, int curr_lane, int pos, vector<vector<int>>& dp) {
+        int n = obstacles.size() - 1;
         // Base case
-        if(pos==n){
+        if (pos == n) {
             return 0;
-        } 
-        if(dp[curr_lane][pos]!=-1) return dp[curr_lane][pos];
-        if(obstacles[pos+1]!=curr_lane)
-        {
-            return solve(obstacles,curr_lane,pos+1,dp);
-        }else{
-            //Obstacles present we have to jump side 
-            int ans = INT_MAX;
-            for(int i=1;i<=3;i++)
-            {
-                if(curr_lane!=i && obstacles[pos]!=i)
-                    ans = min(ans,1+solve(obstacles,i,pos,dp));
-              dp[curr_lane][pos] = ans;
-            }
-              return  dp[curr_lane][pos];
         }
+        if (dp[curr_lane][pos] != -1) return dp[curr_lane][pos];
+
+        if (obstacles[pos + 1] != curr_lane) {
+            dp[curr_lane][pos] = solve(obstacles, curr_lane, pos + 1, dp);
+        } else {
+            // Obstacle present, we have to jump side
+            int ans = INT_MAX;
+            for (int i = 1; i <= 3; i++) {
+                if (curr_lane != i && obstacles[pos] != i) {
+                    ans = min(ans, 1 + solve(obstacles, i, pos, dp));
+                }
+            }
+            dp[curr_lane][pos] = ans;
+        }
+        return dp[curr_lane][pos];
     }
+
+    int solveTab(vector<int> &obstacles) {
+        int n = obstacles.size() - 1;
+        vector<vector<int>> dp(4, vector<int>(obstacles.size(), 1e9));
+        dp[0][n] = 0;
+        dp[1][n] = 0;
+        dp[2][n] = 0;
+        dp[3][n] = 0;
+
+        for (int pos = n - 1; pos >= 0; pos--) {
+            for (int curr_lane = 1; curr_lane <= 3; curr_lane++) {
+                if (obstacles[pos + 1] != curr_lane) {
+                    dp[curr_lane][pos] = dp[curr_lane][pos + 1];
+                } else {
+                    // Obstacle present, we have to jump side
+                    int ans = 1e9;
+                    for (int i = 1; i <= 3; i++) {
+                        if (curr_lane != i && obstacles[pos] != i) {
+                            ans = min(ans, 1 + dp[i][pos + 1]);
+                        }
+                    }
+                    dp[curr_lane][pos] = ans;
+                }
+            }
+        }
+
+        // Return the minimum jumps needed starting from lane 2 at position 0
+        return min(dp[2][0], min(1 + dp[1][0], 1 + dp[3][0]));
+    }
+
     int minSideJumps(vector<int>& obstacles) {
-        vector<vector<int>>dp(4,vector<int>(obstacles.size(),-1));
-        return solve(obstacles,2,0,dp);
+        // Uncomment this line if you want to use the recursive method with memoization.
+        // vector<vector<int>> dp(4, vector<int>(obstacles.size(), -1));
+        // return solve(obstacles, 2, 0, dp);
+
+        // Using the tabulation method by default.
+        return solveTab(obstacles);
     }
 };
