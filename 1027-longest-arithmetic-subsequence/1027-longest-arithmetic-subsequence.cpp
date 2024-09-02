3b7ecd976a354cae4b3eda0eeb1de2292b85abfd
Numan Akhtar <153702609+Numan309@users.noreply.github.com>
2024-09-02 11:27:06 +0530

    Time: 1320 ms (37.79%), Space: 229.2 MB (64.52%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -2,22 +2,25 @@ class Solution {
 public:
     int longestArithSeqLength(vector<int>& nums) {
         int n = nums.size();
-        if(n<=2) return n;
-        int ans = 0; 
-        unordered_map<int,int>dp[n+1];
-        for(int i=1;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            {
-                int diff = nums[i]-nums[j];
+        if (n <= 2)
+            return n;
+
+        int ans = 0;
+        unordered_map<int, int> dp;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int diff = nums[i] - nums[j];
                 int cnt = 1;
-                //check if ans if already present
-                if(dp[j].count(diff))
-                    cnt = dp[j][diff];
-                dp[i][diff] = 1+cnt;
-                ans = max(ans,dp[i][diff]);
+
+                if (dp.count(j * 1000 + diff))
+                    cnt = dp[j * 1000 + diff];
+
+                dp[i * 1000 + diff] = 1 + cnt;
+                ans = std::max(ans, dp[i * 1000 + diff]);
             }
         }
+
         return ans;
     }
 };
