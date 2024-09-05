1fb5127a0d0a2f802b38c007cd44a0837b4f7152
Numan Akhtar <153702609+Numan309@users.noreply.github.com>
2024-09-05 12:20:32 +0530

    Time: 6 ms (51.30%), Space: 15.8 MB (74.67%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,79 +1,14 @@
+
 class Solution {
 public:
-    int solve(int ind,int buy,vector<int>&prices,vector<vector<int>>&dp)
-    {
-        if(ind==prices.size()) return 0;
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
-        int profit = 0;
-        if(buy)
-        {
-            int buyKaro = -prices[ind]+solve(ind+1,0,prices,dp);
-            int skipKaro = 0+solve(ind+1,1,prices,dp);
-            profit = max(buyKaro,skipKaro);
-        }else{
-            int sellKaro = +prices[ind]+solve(ind+1,1,prices,dp);
-            int skipKaro = 0+solve(ind+1,0,prices,dp);
-            profit = max(sellKaro,skipKaro);
-        }
-        return dp[ind][buy] = profit;
-    }
-    int solveTab(vector<int>&prices)
-    {
-          int n = prices.size();
-          vector<vector<int>>dp(n+1,vector<int>(2,0));
-        for(int ind=n-1;ind>=0;ind--)
-        {
-            for(int buy = 0;buy<=1;buy++)
-            {
-                  int profit = 0;
-        if(buy)
-        {
-            int buyKaro = -prices[ind]+dp[ind+1][0];
-            int skipKaro = 0+dp[ind+1][1];
-            profit = max(buyKaro,skipKaro);
-        }else{
-            int sellKaro = +prices[ind]+dp[ind+1][1];
-            int skipKaro = 0+dp[ind+1][0];
-            profit = max(sellKaro,skipKaro);
-        }
-         dp[ind][buy] = profit;
-            }
-        }
-        return dp[0][1];
-    }
-     int solveOpt(vector<int>&prices)
-    {
-          int n = prices.size();
-           vector<int>curr(2,0);
-           vector<int>next(2,0);
-        for(int ind=n-1;ind>=0;ind--)
-        {
-            for(int buy = 0;buy<=1;buy++)
-            {
-                  int profit = 0;
-        if(buy)
-        {
-            int buyKaro = -prices[ind]+next[0];
-            int skipKaro = 0+next[1];
-            profit = max(buyKaro,skipKaro);
-        }else{
-            int sellKaro = +prices[ind]+next[1];
-            int skipKaro = 0+next[0];
-            profit = max(sellKaro,skipKaro);
-        }
-        curr[buy] = profit;
-            }
-            next = curr;
-        }
-        return next[1];
-    }
     int maxProfit(vector<int>& prices) {
-        // int n = prices.size();
-        // vector<vector<int>>dp(n,vector<int>(2,-1));
-        // return solve(0,1,prices,dp);
-        
-       // return solveTab(prices);
-        
-        return solveOpt(prices);
+        int ans = 0;
+        for(int i=0;i<prices.size()-1;i++)
+        {
+            if(prices[i]<prices[i+1])
+              ans += prices[i+1]-prices[i];
+        }
+        return ans;
     }
 };
+
