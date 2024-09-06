e74cfa47af3842e82aa7758aa30a5f972efc89bd
Numan Akhtar <153702609+Numan309@users.noreply.github.com>
2024-09-06 10:55:39 +0530

    Time: 33 ms (33.67%), Space: 26 MB (57.72%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -15,10 +15,33 @@ public:
         }
         return dp[i][j] = ans;
     }
-    int longestCommonSubsequence(string text1, string text2) {
+    int solveTab(string text1,string text2)
+    {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(text1,text2,0,0,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0)); 
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+           int ans = 0;
+        if(text1[i]==text2[j])
+        {
+            ans = 1+dp[i+1][j+1];
+        }else{
+              ans = max(dp[i+1][j],dp[i][j+1]);
+        }
+           dp[i][j] = ans;
+            }
+        }
+        return dp[0][0];
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        // int n = text1.size();
+        // int m = text2.size();
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        // return solve(text1,text2,0,0,dp);
+        
+        return solveTab(text1,text2);
     }
 };
