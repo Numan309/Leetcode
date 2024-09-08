765013da03f7300823eed06f1b15df86d3203da2
Numan Akhtar <153702609+Numan309@users.noreply.github.com>
2024-09-08 17:48:19 +0530

    Time: 25 ms (74.03%), Space: 12.6 MB (18.46%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int>& cuts,vector<vector<int>>&dp) {
-        if (i > j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans = INT_MAX;
-        for (int ind = i; ind <= j; ind++) {
-            // Calculate the cost for the current cut
-            int res = cuts[j + 1] - cuts[i - 1] 
-                    + solve(i, ind - 1, cuts,dp) 
-                    + solve(ind + 1, j, cuts,dp);
-            ans = min(ans, res);
-        }
-        return dp[i][j] = ans;
-    }
+   
 
     int minCost(int n, vector<int>& cuts) {
         int c = cuts.size();
-        cuts.push_back(0);  // Add the starting point
-        cuts.push_back(n);  // Add the ending point
-        sort(cuts.begin(), cuts.end());
-        vector<vector<int>>dp(c+2,vector<int>(c+2,-1));
-        return solve(1, c, cuts,dp);  // Start from the first cut to the last cut
+        cuts.push_back(n);
+    cuts.insert(cuts.begin(), 0);
+    sort(cuts.begin(), cuts.end());
+
+    // Create a DP table to store calculated values.
+    vector<vector<int>> dp(c + 2, vector<int>(c + 2, 0));
+
+    for (int i = c; i >= 1; i--) {
+        for (int j = 1; j <= c; j++) {
+            if (i > j) continue;
+
+            int mini = INT_MAX;
+
+            for (int ind = i; ind <= j; ind++) {
+                // Calculate the cost for making a cut at position 'ind'.
+                int ans = cuts[j + 1] - cuts[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+
+                mini = min(mini, ans);
+            }
+
+            dp[i][j] = mini;
+        }
     }
+
+    return dp[1][c];
+}
+    
 };
