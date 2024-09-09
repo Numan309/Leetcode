8a0940ce120bf9b4ebd553c32ce017efb3191b1c
Numan Akhtar <153702609+Numan309@users.noreply.github.com>
2024-09-09 10:58:36 +0530

    Time: 42 ms (50.28%), Space: 32 MB (11.17%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,13 +3,13 @@ public:
      int solve(string &pattern,string &str,int i,int j,
               vector<vector<int>>&dp)
     {
-        if(i<0 && j<0) return 1;
-        if(i>=0 && j<0) return 0;
-        if(i<0 && j>=0)
+        if(i==0 && j==0) return 1;
+        if(i>0 && j==0) return 0;
+        if(i==0 && j>0)
         {
-            for(int k=0;k<=j;k++)
+            for(int k=1;k<=j;k++)
             {
-                if(pattern[k]!='*')
+                if(pattern[k-1]!='*')
                 {
                     return 0;
                 }
@@ -18,10 +18,10 @@ public:
         }
          if(dp[i][j]!=-1) return dp[i][j];
         //Match
-        if(str[i]==pattern[j] || pattern[j]=='?')
+        if(str[i-1]==pattern[j-1] || pattern[j-1]=='?')
         
             return dp[i][j] = solve(pattern,str,i-1,j-1,dp);
-        else if(pattern[j]=='*')
+        else if(pattern[j-1]=='*')
         
       return dp[i][j] = (solve(pattern,str,i-1,j,dp) || solve(pattern,str,i,j-1,dp));
         else
@@ -31,7 +31,7 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-       vector<vector<int>>dp(n,vector<int>(m,-1));
-        return solve(p,s,n-1,m-1,dp);
+       vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solve(p,s,n,m,dp);
     }
 };
