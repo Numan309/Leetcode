7eb52305331686f5dc2862b50074c58423be5ced
Numan Akhtar <153702609+Numan309@users.noreply.github.com>
2024-09-25 18:36:29 +0530

    Time: 257 ms (21.44%), Space: 202.5 MB (5.11%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int n;
-    long long dp[100001];
-    long long solve(vector<vector<int>>&questions,int i)
-    {
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-    long long take = questions[i][0]+solve(questions,i+questions[i][1]+1);
-        long long notTake = solve(questions,i+1);
-       long long ans = max(take,notTake);
-        return dp[i] = ans;
-    }
+
     long long mostPoints(vector<vector<int>>& questions) {
-         n = questions.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(questions,0);
+        int n = questions.size();
+        
+        if(n == 1)
+            return questions[0][0];
+        
+        vector<long long> t(200001);
+        //t[i] = Max points gained by Questions from questions[i to n-1]
+        //return t[0] - from 0 to n-1
+
+        
+        for(int i = n-1; i >= 0; i--) {
+      t[i] = max(questions[i][0] + t[i + questions[i][1] + 1], t[i+1]);
+        }
+        
+        return t[0];
     }
 };
