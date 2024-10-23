81ac4c67fad0dea30dd47e5aaa62ab4045b6853b
Numan Akhtar <153702609+Numan309@users.noreply.github.com>
2024-10-23 21:38:59 +0530

    Time: 191 ms (18.21%), Space: 9.7 MB (12.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-   vector<int>dp;
+  vector<int>dp;
+ //   int n[30];
     int fibonacci(int n)
     {
           if(n==0) return 0;
         if(n==1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=fib(n-1)+fib(n-2);
+       if(dp[n]!=-1) return dp[n];
+        return dp[n]= fib(n-1)+fib(n-2);
     }
     int fib(int n) {
-        // memset(dp,-1,sizeof(dp));
+      //   memset(dp,-1,sizeof(dp));
         dp.assign(n+1,-1);
       return fibonacci(n);   
     }
